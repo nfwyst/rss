0e969874c880b8c652870469ddcd6416faf755cd
nfwyst <nfwyst@gmail.com>
2018-08-10 21:20:48 +0800

    define interface for menu & implement menu data service

---
+++ app/ts/interfaces/menu.ts
@@ -0,0 +1,20 @@
+// 菜单项接口
+export interface MenuItem {
+  url: string,
+  title: string,
+  id: string,
+}
+
+// 菜单接口
+export interface TMenu {
+  // 菜单的所有菜单项
+  items: Array<MenuItem>;
+  // 清除方法
+  clear(): void;
+  // 删除菜单项
+  remove(url: string): Array<MenuItem>;
+  // 添加菜单项
+  add(url: string, title: string): Array<MenuItem>;
+  // 初始化从 localStorage 加载
+  load(): Array<MenuItem>;
+}
---
+++ app/ts/services/menu.ts
@@ -0,0 +1,33 @@
+import sha1 from 'sha1';
+import { MenuItem, TMenu } from '../interfaces/menu';
+
+export default class Menu implements TMenu {
+  constructor(private ns: string) {}
+  /**
+   * 私有方法, 保存菜单数据到 localStorage
+   */
+  private save(): void {
+    localStorage.setItem(this.ns, JSON.stringify(this.items));
+  }
+  items: Array<MenuItem> = [];
+  clear(): void {
+    this.items = [];
+    this.save();
+  }
+  remove(url: string): Array<MenuItem> {
+    this.items = this.items.filter(item => item.url !== url);
+    this.save();
+    return this.items;
+  }
+  add(url: string, title: string): Array<MenuItem> {
+    // 根据 url 生成 id
+    const id = <string>sha1(url);
+    this.items.push({id, url, title});
+    this.save();
+    return this.items;
+  }
+  load(): Array<MenuItem> {
+    this.items = JSON.parse(localStorage.getItem(this.ns) || '[]');
+    return this.items;
+  }
+}
